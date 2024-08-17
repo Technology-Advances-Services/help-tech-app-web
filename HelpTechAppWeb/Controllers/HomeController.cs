526cbc1ce095f8bbba567be28be556e91ac7a16c
aaron acuña <aaronacu_85@hotmail.com>
2024-08-17 14:34:00 -0500

    feat(home): implemented restriction for controller.

--- HelpTechAppWeb/Controllers/HomeController.cs
+++ HelpTechAppWeb/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelpTechAppWeb.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public IActionResult Index()
