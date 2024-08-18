ab39cdf889a9e99bb0d4e637e522bb4d7a34bf43
aaron acuña <aaronacu_85@hotmail.com>
2024-08-18 17:54:58 -0500

    feat(project): ordered references.

--- HelpTechAppWeb/Program.cs
+++ HelpTechAppWeb/Program.cs
@@ -1,6 +1,6 @@
-using HelpTechAppWeb.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Options;
+using HelpTechAppWeb.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
