5ae402b4a17e712a4e61bb5bfeecfac6bc0f7128
aaron acuña <aaronacu_85@hotmail.com>
2024-08-18 15:42:05 -0500

    feat(access): added endpoint json for login view and user model.

--- HelpTechAppWeb/Controllers/AccessController.cs
+++ HelpTechAppWeb/Controllers/AccessController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using HelpTechAppWeb.Models;
 
 namespace HelpTechAppWeb.Controllers
 {
@@ -29,17 +32,11 @@ namespace HelpTechAppWeb.Controllers
 
         [Route("register-technical")]
         [HttpGet]
-        public IActionResult RegisterTechnical()
-        {
-            return View();
-        }
+        public IActionResult RegisterTechnical() => View();
 
         [Route("register-consumer")]
         [HttpGet]
-        public IActionResult RegisterConsumer()
-        {
-            return View();
-        }
+        public IActionResult RegisterConsumer() => View();
 
         [HttpGet]
         public async Task<IActionResult> Logout()
@@ -49,6 +46,30 @@ namespace HelpTechAppWeb.Controllers
                 .AuthenticationScheme);
 
             return RedirectToAction("Login", "Access");
+        }
+
+        #endregion
+
+        #region Json
+
+        [HttpPost]
+        public async Task<IActionResult> Login
+            ([FromBody] User user)
+        {
+            var httpContent = new StringContent(JsonConvert.SerializeObject(user),
+                Encoding.UTF8, "application/json");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new("Bearer", "");
+
+            var httpResponseMessage = await _httpClient.PostAsync("", httpContent);
+
+            if (httpResponseMessage.IsSuccessStatusCode is false)
+                return RedirectToAction("Error","Home");
+
+            var result = await httpResponseMessage
+                .Content.ReadAsStringAsync();
+
+            return Content(result, "application/json");
         }
 
         #endregion
---
+++ HelpTechAppWeb/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class User
+    {
+        public int Username { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
+
+        public User()
+        {
+            this.Username = 0;
+            this.Password = string.Empty;
+            this.Role = string.Empty;
+        }
+        public User
+            (int username, string password,
+            string role)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Role = role;
+        }
+    }
+}
