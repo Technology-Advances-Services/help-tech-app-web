7f4020a3179243fd1390bd0435552c604b51c464
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 18:32:44 -0500

    feat(project): added specialty model.

---
+++ HelpTechAppWeb/Models/Specialty.cs
@@ -0,0 +1,20 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class Specialty
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Specialty()
+        {
+            this.Id = 0;
+            this.Name = string.Empty;
+        }
+        public Specialty
+            (int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
