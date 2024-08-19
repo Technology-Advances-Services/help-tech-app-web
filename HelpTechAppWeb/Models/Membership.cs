92bbdb929107ef458f487b0604838b8a2fa900e7
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 18:24:34 -0500

    feat(project): added membership model.

---
+++ HelpTechAppWeb/Models/Membership.cs
@@ -0,0 +1,27 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class Membership
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string Policies { get; private set; }
+
+        public Membership()
+        {
+            this.Id = 0;
+            this.Name = string.Empty;
+            this.Price = 0m;
+            this.Policies = string.Empty;
+        }
+        public Membership
+            (int id, string name,
+            decimal price, string policies)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Policies = policies;
+        }
+    }
+}
