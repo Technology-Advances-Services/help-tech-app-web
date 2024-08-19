bd00e81b7c4156b06fa1e6884d29c5038474910c
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 17:55:52 -0500

    feat(technical): implemented style for interfaz technical view.

--- HelpTechAppWeb/Controllers/TechnicalController.cs
+++ HelpTechAppWeb/Controllers/TechnicalController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace HelpTechAppWeb.Controllers
 {
+    [Authorize(Roles = "TECNICO")]
     public class TechnicalController : Controller
     {
+        private ClaimsPrincipal? _claimsPrincipal;
+
+        #region Views
         public IActionResult InterfaceTechnical()
         {
+            _claimsPrincipal = HttpContext.User;
+
+            if (_claimsPrincipal.FindFirst
+                (ClaimTypes.Name)?.Value == null)
+                return RedirectToAction("Error", "Home");
+
+            ViewBag.TechnicalId = _claimsPrincipal
+                .FindFirst(ClaimTypes.Name)?.Value;
+
             return View();
         }
+
+        #endregion
     }
 }
