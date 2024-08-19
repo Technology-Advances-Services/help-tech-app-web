bf27b1fb6c616ebbe6ebc51de2fe951a6fd2b7b2
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 18:22:57 -0500

    feat(project): added type complaint model.

---
+++ HelpTechAppWeb/Models/TypeComplaint.cs
@@ -0,0 +1,20 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class TypeComplaint
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public TypeComplaint()
+        {
+            this.Id = 0;
+            this.Name = string.Empty;
+        }
+        public TypeComplaint
+            (int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
