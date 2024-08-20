38c08affe8f6719ec248fb922dcd245dddf1467c
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 19:38:47 -0500

    feat(project): added chat member model.

---
+++ HelpTechAppWeb/Models/ChatMember.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class ChatMember
+    {
+        public int ChatRoomId { get; private set; }
+        public int? TechnicalId { get; private set; }
+        public int? ConsumerId { get; private set; }
+
+        public ChatMember()
+        {
+            this.ChatRoomId = 0;
+            this.TechnicalId = null;
+            this.ConsumerId = null;
+        }
+        public ChatMember
+            (int chatRoomId, int? technicalId,
+            int? consumerId)
+        {
+            this.ChatRoomId = chatRoomId;
+            this.TechnicalId = technicalId;
+            this.ConsumerId = consumerId;
+        }
+    }
+}
