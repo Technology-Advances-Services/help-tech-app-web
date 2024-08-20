aa591b99f0080a11d898a53a9b73977214b77687
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 19:48:55 -0500

    feat(project): added district model.

---
+++ HelpTechAppWeb/Models/District.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class District
+    {
+        public int Id { get; private set; }
+        public int DepartmentId { get; private set; }
+        public string Name { get; private set; }
+
+        public District()
+        {
+            this.Id = 0;
+            this.DepartmentId = 0;
+            this.Name = string.Empty;
+        }
+        public District
+            (int id, int departmentId,
+            string name)
+        {
+            this.Id = id;
+            this.DepartmentId = departmentId;
+            this.Name = name;
+        }
+    }
+}
