62095663ae8c6d6e614edee06dc141ec958514b6
aaron acuña <aaronacu_85@hotmail.com>
2024-08-19 19:48:02 -0500

    feat(project): added department model.

---
+++ HelpTechAppWeb/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class Department
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Department()
+        {
+            this.Id = 0;
+            this.Name = string.Empty;
+        }
+        public Department
+            (int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
