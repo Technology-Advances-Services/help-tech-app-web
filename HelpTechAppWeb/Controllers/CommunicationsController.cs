6f748f83137df3aef8804cb532a688932599edee
aaron acuña <aaronacu_85@hotmail.com>
2024-08-24 15:05:30 -0500

    fea(communications): added controller and messaging view.

---
+++ HelpTechAppWeb/Controllers/CommunicationsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HelpTechAppWeb.Configurations.Interfaces;
+
+namespace HelpTechAppWeb.Controllers
+{
+    [Route("communications/")]
+    [Authorize(Roles = "TECNICO,CONSUMIDOR")]
+    public class CommunicationsController
+        (IBaseRequest baseRequest) : Controller
+    {
+        #region Views
+
+        [Route("messaging")]
+        [HttpGet]
+        public IActionResult Messaging
+            (int personId, string role)
+        {
+            ViewBag.ChatRoomId = 0;
+            ViewBag.Role = role;
+            ViewBag.PersonId = personId;
+
+            return View();
+        }
+
+        #endregion
+
+        #region Json
+
+
+
+        #endregion
+    }
+}
