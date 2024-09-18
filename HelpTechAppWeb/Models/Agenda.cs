97f6a56a681f240ba871882d3104d53dd9ab2c83
aaron acuña <aaronacu_85@hotmail.com>
2024-09-17 20:16:18 -0500

    feat(project): added new properties for agenda model.

---
+++ HelpTechAppWeb/Models/Agenda.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpTechAppWeb.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public string TechnicalId {  get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public Agenda()
+        {
+            this.Id = 0;
+            this.TechnicalId = string.Empty;
+            this.RegistrationDate = DateTime.Now;
+        }
+        public Agenda
+            (int id, string technicalId,
+            DateTime registrationDate)
+        {
+            this.Id = id;
+            this.TechnicalId = technicalId;
+            this.RegistrationDate = registrationDate;
+        }
+    }
+}
